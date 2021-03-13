db0ed1268bd704ece05ff2e86eb3d59affb1681f
BenjaminParment <parment.benjamin@gmail.com>
2021-03-13 15:44:38 +0000

    Adds eslint and prettier with standard rules
    Refactors the whole project due to the change in rules.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import './App.scss';
+import "./App.scss";
 
 import React, { Component } from "react";
 
-import { setInput } from './redux/input/input.actions';
-import { connect } from 'react-redux';
+import { setInput } from "./redux/input/input.actions";
+import { connect } from "react-redux";
 
-const mapStateToProps = state => {
-  return {
-    input: state.input.content,
-  }
-}
+const mapStateToProps = (state) => {
+    return {
+        input: state.input.content,
+    };
+};
 
-const mapDispatchToProps = dispatch => {
-  return {
-    set:(content) => dispatch(setInput(content)),
-  }
-}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        set: (content) => dispatch(setInput(content)),
+    };
+};
 
 export class App extends Component {
-  
-  onChange = (e) => {
-    this.props.set(e.target.value);
-  }
-  
-  render = () => (
-      <div className="App">
-          <input onChange={this.onChange}></input>
-      </div>
-  );
+    onChange = (e) => {
+        this.props.set(e.target.value);
+    };
+
+    render = () => (
+        <div className="App">
+            <input onChange={this.onChange}></input>
+        </div>
+    );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
